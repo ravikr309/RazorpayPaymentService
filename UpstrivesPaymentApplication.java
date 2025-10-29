c572707ce7647fb94ec659515c935b9f07c53d35
Ravi Kumar <119651139+ravikr309@users.noreply.github.com>
2025-10-30 03:43:40 +0530

    Rename

--- UpstrivesPaymentApplication.java
+++ UpstrivesPaymentApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UpstrivesPaymentApplication {
+public class PaymentApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UpstrivesPaymentApplication.class, args);
+		SpringApplication.run(PaymentApplication.class, args);
 	}
 
 }
+
